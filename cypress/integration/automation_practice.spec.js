ddef9a56b0501b1b9e0a63aa37d502a041e9fa60
bb <bb@com.pl>
2019-07-04 09:51:24 +0200

    Day4: test for filter checkboxes - category page

--- cypress/integration/automation_practice.spec.js
+++ cypress/integration/automation_practice.spec.js
@@ -91,5 +91,34 @@ context("automationpractice website testing", () => {
     })
 
   })
+
+  describe('Category page tests', () => {
+
+    it.only('Category page - verify if all filter checkboxes can be checked and unchecked', () => {
+
+      //Click first category from menu on main page
+      cy.get('.sf-with-ul').first().click()
+
+      //Verify if User was redirected to the new page
+      cy.url().should('include', 'controller=category');
+
+      //Get all filters checkkoxes.
+      //Check all checkboxes. 
+      //Get parent of each checkbox. 
+      //Verify if parent of each checkbox has class 'checked' which is added when element is checked.
+      cy.get('.checkbox').check().parent().should('have.class', 'checked')
+
+
+      //Get all filters checkkoxes.
+      //Uncheck all checkboxes. 
+      //Get parent of each checkbox. 
+      //Verify if parent of each checkbox doesn't have class 'checked' which is removed when element is unchecked.
+      cy.get('.checkbox').uncheck().parent().should('not.have.class', 'checked')
+
+    })
+
+  })
+
+  
 })
 
