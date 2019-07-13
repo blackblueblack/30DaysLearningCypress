11e86c1c007cc7af272cc3741a606af5e076b808
bb <bb@com.pl>
2019-07-13 15:26:51 +0200

    Day13: Cucumber DataTable example

--- cypress/integration/cucumberdemo/demo.feature
+++ cypress/integration/cucumberdemo/demo.feature
@@ -1,10 +1,18 @@
-Feature: Website Header - Product search field 
+Feature: Website Header - Product search field
 
-  Header: Verify if autocomplete drop-down list for search input field is selectable
-  
+Header: Verify if autocomplete drop-down list for search input field is selectable
+
   Scenario: Selecting element from autocomplete drop-down
     Given I open practiceautomation site
     When I type product name in search input field
     Then I can click element on autocomplete drop-down
-    And I am redirected to procut site 
-    
+    And I am redirected to product site
+
+  Scenario: Valid and invalid search input
+    Given I submit valid or inalid product name
+      | product |
+      | Dress   |
+      | skirt   |
+      | XXX123%_@ |
+      |  1234567890123456789012345789QWERTYqwertyzxcvbnmlkjhg hhjkkkkkk3456789|
+      | ' ' |
--- cypress/integration/cucumberdemo/demo/demo.js
+++ cypress/integration/cucumberdemo/demo/demo.js
@@ -2,21 +2,36 @@ import { Given, And, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 const url = 'http://automationpractice.com/index.php';
 
+
 Given('I open practiceautomation site', () => {
   cy.visit(url)
 })
 
 When('I type product name in search input field', () => {
-  cy.get('#search_query_top').type('dress');
+
+    cy.get('#search_query_top').type("Dress")
 
 })
 
 Then('I can click element on autocomplete drop-down', () => {
   cy.get('.ac_results').find('li').contains('Dress').click()
 })
-
-And('I am redirected to procut site', () => {
+And('I am redirected to product site', () => {
   cy.url().should('include', 'controller=product');
+})
+
+Given('I submit valid or inalid product name', datatable => {
+
+  const table = datatable.hashes();
+  table.forEach(row => {
+    cy.visit('url');
+    cy.get('#search_query_top').type(row.product).click();
+    cy.get('#searchbox > .btn').click();
+    cy.get('#search_query_top').clear();
+    cy.url().should('include', 'submit_search=');
+    cy.get('h1.page-heading').should('contain', 'Search')
+    
+  })
 })
 
 
