c6c510e3083bf1891a0f44e01f2badcacac8a8bc
bb <bb@com.pl>
2019-07-13 17:25:16 +0200

    Adding README file

---
+++ README.md
@@ -0,0 +1,7 @@
+### 30DaysLearningCypress 
+As the name of the project says, its goal is to learn Cypress basics for 30 days (ideally consecutive).  
+
+Each day of learning includes: 
+- writing some code (or refactoring code according new concept), 
+- pushing changes on GitHub,
+- writing small learning note and publishing it on blog: https://crazytesting.pl
--- cypress/integration/cucumberdemo/demo/demo.js
+++ cypress/integration/cucumberdemo/demo/demo.js
@@ -2,15 +2,13 @@ import { Given, And, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 const url = 'http://automationpractice.com/index.php';
 
-
-Given('I open practiceautomation site', () => {
+Given('I open automationpractice site', () => {
   cy.visit(url)
 })
 
 When('I type product name in search input field', () => {
 
     cy.get('#search_query_top').type("Dress")
-
 })
 
 Then('I can click element on autocomplete drop-down', () => {
@@ -25,12 +23,11 @@ Given('I submit valid or inalid product name', datatable => {
   const table = datatable.hashes();
   table.forEach(row => {
     cy.visit('url');
-    cy.get('#search_query_top').type(row.product).click();
+    cy.get('#search_query_top').type(row.product);
     cy.get('#searchbox > .btn').click();
     cy.get('#search_query_top').clear();
     cy.url().should('include', 'submit_search=');
     cy.get('h1.page-heading').should('contain', 'Search')
-    
   })
 })
 
