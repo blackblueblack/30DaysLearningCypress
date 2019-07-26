3fb410c52fc976e6d517913aa8978fed0e6885eb
bb <bb@com.pl>
2019-07-26 12:21:02 +0200

    Day23: Select random element from select menu

--- cypress/integration/special_UI_elements.spec.js
+++ cypress/integration/special_UI_elements.spec.js
@@ -1,37 +1,63 @@
 /// <reference types="Cypress" />
 
-//Since tests for different website are mixed in this spec.js,  organization of tests can differ form best practices. The goal of this file is to collect testing excercises for different UI elements and different approach. 
+//The goal of this file is to collect testing excercises for different UI elements and different approach. 
+//Tests for different website are mixed in this spec.js and their organization can differ form best practices.
+
 
 context('Special UI elements testing', () => {
-
-  it('Slider testing', () => {
-    cy.visit('https://the-internet.herokuapp.com/horizontal_slider');
-    //Get slider and invoke 'val' method on it. 
-    //Val method is invoked with integer value which is expected to be selected on slider. 
-    //Trigger command triggers event ('change') on slider value.
-    cy.get('input[type=range]').invoke('val', 5).trigger('change');
-    //Assert text value displayed on slider. 
-    cy.get('#range').should('have.text', '5');
+  describe('Slider tests', () => {
+    it('Selecting value on slider', () => {
+      cy.visit('https://the-internet.herokuapp.com/horizontal_slider');
+      //Get slider and invoke 'val' method on it. 
+      //Val method is invoked with integer value which is expected to be selected on slider. 
+      //Trigger command triggers event ('change') on slider value.
+      cy.get('input[type=range]').invoke('val', 5).trigger('change');
+      //Assert text value displayed on slider. 
+      cy.get('#range').should('have.text', '5');
+    })
   })
 
-  it('Compare DOM options list with required list', () => {
-    //Getting data file and aliasing it for later usage (data file contains required list of speed options)
-    cy.fixture('demoqa_site_data').as('demoqa_site_data')
+  describe('Select menu testing', () => {
 
-    cy.visit('https://demoqa.com/selectmenu/');
-    //Gettting aliased data and returning it with then() callback function. 
-    cy.get('@demoqa_site_data').then((demoqadata) => {
+    beforeEach( ()=> {
+      
+      cy.visit('https://demoqa.com/selectmenu/');
 
-      //Variable which stores required speed options (taken from fixture file). 
-      const requiredSpeedOptions = demoqadata.speedOptionsList;
-      //Variable which collects speed options retrieved from DOM list. 
-      var availableSpeedOptions = [];
-      //Loop which collect option's text and append it to the list. 
-      cy.get('#speed>option').each(($elem) => {
-        availableSpeedOptions.push($elem.text())
-      }).then(() => {
-        //Assert if list of DOM speed options is in accordance with required speed list option. 
-        expect(availableSpeedOptions).to.be.eql(requiredSpeedOptions)
+      //Getting data file and aliasing it for later usage (data file contains required list of speed options).
+      cy.fixture('demoqa_site_data').as('demoqa_site_data')
+    });
+
+    it('Compare DOM options list with required list', () => {
+      //Gettting aliased data and returning it with then() callback function. 
+      cy.get('@demoqa_site_data').then((demoqadata) => {
+        //Variable which stores required speed options (taken from fixture file). 
+        const requiredSpeedOptions = demoqadata.speedOptionsList;
+        //Variable which collects speed options retrieved from DOM list. 
+        var availableSpeedOptions = [];
+        //Loop which collect option's text and append it to the list. 
+        cy.get('#speed>option').each(($elem) => {
+          availableSpeedOptions.push($elem.text())
+        }).then(() => {
+          //Assert if list of DOM speed options is in accordance with required speed list option. 
+          expect(availableSpeedOptions).to.be.eql(requiredSpeedOptions)
+        })
+      })
+    })
+
+    it('Select random element from select menu', () => {
+      //Use demoqa site data - return it as then() callbackfunction. 
+      cy.get('@demoqa_site_data').then((demoqadata) => {
+        //Variable which stores speed value randomy selected from demoqa site data. 
+          let randomSpeedValue = Cypress._.sample(demoqadata.speedOptionsList) 
+        //Click on select menu.    
+          cy.get('#speed-button').click()
+        //Assert if expanded select menu list is visible. 
+          cy.get('#speed-menu').should('be.visible')
+        //Click speed value.
+          cy.get('li').contains(randomSpeedValue).click().then(() => {
+        //Assert if expanded select menu list is not visible. 
+          cy.get('#speed-menu').should('not.be.visible')
+          })
       })
     })
   })
